5688f673d10e56c433eb8ba37303b9fde9fd1b6f
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-09-11 16:15:29 -0600

    basic up or down vote button built

--- src/components/VoteButton.js
+++ src/components/VoteButton.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function VoteButton(props)
+function VoteButton({type, num})
 {
+    if (type === undefined || type === null) throw new Error("type must be UP or DOWN!");
+    else
+    {
+        if (type === "Up" || type === "UP" || type === "DOWN" || type === "Down");
+        else throw new Error("type must be UP or DOWN!");
+    }
+    const typeisup = (type === "Up" || type === "UP");
+
     return (
-        null
+        <button onClick={null}>{typeisup ? "👍": "👎"} {num}</button>
     );
 }
 
